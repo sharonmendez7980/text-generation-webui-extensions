6ec6a158e54a2ff651379291bef4946c70339e64
PixelSmith <140158923+sharonmendez7980@users.noreply.github.com>
2024-08-17 10:16:23 +0900

    Update (8)SillyStringUtils.sol

--- (8)SillyStringUtils.sol
+++ (8)SillyStringUtils.sol
@@ -8,6 +8,11 @@ library SillyStringUtils {
         string line1;
         string line2;
         string line3;
+string line4;
+string line5;
+string line6;
+string line7;
+string line8;
     }
 
     function shruggie(string memory _input) internal pure returns (string memory) {
